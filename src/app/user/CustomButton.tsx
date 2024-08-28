81f2fc0226822bd2ae032a6c7664450d37e986bf
zeng <ethz0607@gmail.com>
2024-08-28 15:13:48 +0800

    feat: client

--- src/app/user/CustomButton.tsx
+++ src/app/user/CustomButton.tsx
@@ -1,14 +1,23 @@
 'use client'
 import { Button } from 'antd'
+import { Metadata } from 'next'
 import { useRouter } from 'next/navigation'
+
 
 export default function CustomButton(props: any) {
     const router = useRouter()
     const goHome = () => {
         router.push('/')
     }
+    // 改变标题
+    const changeTitle = () => {
+        document.title = 'New Title'
+    }
 
     return (
-        <Button type="primary" onClick={goHome}>Go Home</Button>
+        <>
+            <Button type="primary" onClick={goHome}>Go Home</Button>
+            <Button type="primary" onClick={changeTitle}>Change Title</Button>
+        </>
     )
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,23 +3,18 @@ import type { Metadata } from "next"
 import { redirect } from "next/navigation"
 import CustomButton from "./CustomButton"
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
     title: "User",
 }
 
+
 export default function User() {
-
-
-    const changeTitle = ()=>{
-        metadata.title = "New Title"
-    }
-
-    const isGoHome = true
+    const isGoHome = false
 
     isGoHome && redirect("/")
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between">
+        <main className="flex min-h-screen flex-col items-center">
             <CustomButton />
         </main>
     )
